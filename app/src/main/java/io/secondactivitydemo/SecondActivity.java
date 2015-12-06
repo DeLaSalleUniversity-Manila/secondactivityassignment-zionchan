e2bd8468597b6ce9f7f77f5f3300973bc44960b4
zionchan <zion_chan@dlsu.edu.ph>
2015-12-06 20:57:02 +0800

    yay

---
+++ app/src/main/java/io/secondactivitydemo/SecondActivity.java
@@ -0,0 +1,26 @@
+package io.secondactivitydemo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+
+public class SecondActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+        handleIntent();
+    }
+
+    private void handleIntent(){
+        Intent i = getIntent();
+        String day_out = i.getStringExtra("message");
+        TextView tv_out = (TextView) findViewById(R.id.textViewOut);
+        tv_out.setText(day_out);
+    }
+
+}
